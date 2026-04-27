42664920d135ebcf5892a017180c12bad58c59ae
A41nma <andrescc2817@gmail.com>
2026-04-27 09:34:59 -0400

    organismo creado

--- components/organismo/Helper-card.tsx
+++ components/organismo/Helper-card.tsx
@@ -1,6 +1,6 @@
 import '.Helper-card.css'
 import Description from '../moleculas/descripcion';
-impoert Header from '../moleculas/header';
+import Header from '../moleculas/header';
 export default function Helper-card() {
   return (
     <div className="card">
