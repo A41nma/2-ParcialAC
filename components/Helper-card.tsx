9ca2c6272354faa09a6bd4bed2074880d58511eb
A41nma <andrescc2817@gmail.com>
2026-04-27 09:28:50 -0400

    moleculas agregadas

---
+++ components/Helper-card.tsx
@@ -0,0 +1,9 @@
+import '.Helper-card.css'
+export default function Helper-card() {
+  return (
+    <div className="card">
+      <header/>
+      <description/>
+    </div>
+  );
+}
---
+++ components/moleculas/descripcion.tsx
@@ -0,0 +1,13 @@
+import './descripcion.css';
+ function Descripcion() {
+    return (
+        <div className="descripcion">
+            <img src=''></img>
+            <h2>Descripción</h2>
+            <p>
+                Soy un estudiante de ingeniería en informática con una pasión por la tecnología y el desarrollo de software. Me encanta aprender nuevas habilidades y enfrentar desafíos técnicos. Mi objetivo es convertirme en un desarrollador de software competente y contribuir a proyectos innovadores en el futuro.
+            </p>
+        </div>
+    );
+}
+export default Descripcion;
